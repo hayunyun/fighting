34003b1cfd48b10c28e6b1b5ae452c36397bd905
yuminko <yuk041@ucsd.edu>
2019-06-30 05:13:03 +0900

    classes

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import lessons.views
+import users.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',lessons.views.home, name="home"),
+    path('home',lessons.views.home, name="home"),
+    path('classes',users.views.classes, name="classes"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def classes(request):
+    return render(request, 'classes.html')
