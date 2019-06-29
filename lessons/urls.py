aa20eff55508b20da036dd2ab281c2d29e756e79
Yunyoung Ha <hayunyun@naver.com>
2019-06-29 21:50:11 +0000

    에라모르겠따~

--- lessons/urls.py
+++ lessons/urls.py
@@ -5,7 +5,7 @@ from . import views
  
 app_name = "lessons"
 urlpatterns = [
-    path('<int:id>/', views.show, name="show"),
-    path('', views.home, name="home"),
+    # path('<int:id>/', views.show, name="show"),
+    # path('', views.home, name="home"),
 
 ]
