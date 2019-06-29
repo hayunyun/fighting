31cd6a11b523d8abe2bba36bb1dc4cb80c0516d5
blueberry333 <hiyeahin@gmail.com>
2019-06-30 04:55:23 +0900

    yein

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render, redirect, get_object_or_404
+# from .models import *
+# from django.contrib.auth.models import User
+# # Create your views here.
 
-# Create your views here.
+# def show(request, id):
+#     lessons = get_object_or_404(Order, pk=id)
+#     return render(request, 'lessons/show.html', {"lessons": lessons})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.contrib.auth.models import User
+from lessons .models import *
 # Create your views here.
+
+def home(request, id):
+    user = get_object_or_404(User, pk=id)
+    lessonsOfTeacher = Lesson.objects.all()
+    lessonsOfStudent = Order.objects.all()
+    return render(request, 'home.html', {'user': user, 'lessonsOfTeacher': lessonsOfTeacher, 'lessonsOfStudent': lessonsOfStudent})
