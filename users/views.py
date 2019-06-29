50189e95bdde4b56377ff7c82512846e616d7c8e
blueberry333 <hiyeahin@gmail.com>
2019-06-30 05:31:25 +0900

    yein22

--- users/views.py
+++ users/views.py
@@ -4,11 +4,9 @@ from django.contrib.auth.models import User
 from lessons .models import *
 # Create your views here.
 
-<<<<<<< HEAD
 def home(request, id):
     user = get_object_or_404(User, pk=id)
     lessonsOfTeacher = Lesson.objects.all()
     lessonsOfStudent = Order.objects.all()
     return render(request, 'home.html', {'user': user, 'lessonsOfTeacher': lessonsOfTeacher, 'lessonsOfStudent': lessonsOfStudent})
-=======
->>>>>>> d1a6b823dc4e79ad401774708efbc4a1f214ca5e
+
